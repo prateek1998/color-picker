995e9dce055b35640cdb8213ae21fb0722162b5d
prateek1998 <37973089+prateek1998@users.noreply.github.com>
2020-07-05 21:37:41 +0530

    first version

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Colors from "./colors";
+import ContextProvider from "./Context";
+function App() {
+  return (
+    <div>
+      <ContextProvider>
+        <Colors />
+      </ContextProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Context } from "./Context";
+const Nav = () => {
+  const { colorOption, updateOption } = React.useContext(
+    Context
+  );
+  console.log(colorOption);
+  return (
+    <nav>
+      <ul>
+        <li className="logo">Color Platte</li>
+        <li>
+          <select value={colorOption} onChange={updateOption}>
+            <option value="hex">HEX</option>
+            <option value="rgb">RGB</option>
+            {/* <option>RGBA</option> */}
+          </select>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
